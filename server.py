053223b10056925462c9d3f7fb011426eb11a1a2
SPC0D3 <pete4790@mylaurier.ca>
2024-10-10 10:36:44 -0400

    Modified client and server python files

--- server.py
+++ server.py
@@ -1,131 +1,94 @@
-# Saad Rehan, 210723520
-# Rajanbir Bhitewid, 210361510
+# Sohum Pete 210354790 
+#
 
 # Import socket module
-from socket import * 
-import sys # In order to terminate the program
-import struct
+import socket
+import threading
+import datetime
 
-# Assign a port number
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
+# Server Configuration
+HOST = 'localhost'
+PORT = 12345
+MAX_CLIENTS = 3
 
-# Bind the socket to server address and server port
-serverSocket.bind(("", serverPort))
+# Cache to store client information (name, connection/disconnection times)
+clients_cache = {}
 
+# Repository of available files (for demonstration purposes)
+file_repository = ['file1.txt', 'file2.txt', 'file3.txt']
 
-print("SERVER: ----------Start of Stage A----------")
-#Receiving client-side packet
-packet, clientAddress = serverSocket.recvfrom(1024)
+def handle_client(client_socket, client_name):
+    # Store connection time in cache
+    clients_cache[client_name] = {
+        'connected_at': datetime.datetime.now(),
+        'disconnected_at': None
+    }
 
-data_length, pcode, entity, data = struct.unpack("!IHH14sxx", (packet))
+    print(f"{client_name} connected.")
 
-padding = (4 - (len(data) % 4)) % 4
-data+=bytearray(padding)
+    while True:
+        try:
+            # Receive message from client
+            data = client_socket.recv(1024).decode()
+            if not data:
+                break
 
-packet_length = len(packet)
-print("Receiving from client: data: {} data_length: {} pcode: {} entity: {}".format(data, data_length,pcode, entity))
+            # Handle different types of client requests
+            if data == "status":
+                # Send server cache details
+                cache_info = "\n".join([f"{name}: connected at {details['connected_at']}, disconnected at {details['disconnected_at']}"
+                                        for name, details in clients_cache.items()])
+                client_socket.send(cache_info.encode())
+            elif data == "list":
+                # Send list of available files in repository
+                file_list = "\n".join(file_repository)
+                client_socket.send(file_list.encode())
+            elif data.startswith("get "):
+                # Handle file request
+                requested_file = data.split(" ", 1)[1]
+                if requested_file in file_repository:
+                    client_socket.send(f"Sending contents of {requested_file}".encode())
+                else:
+                    client_socket.send("File not found".encode())
+            elif data == "exit":
+                # Handle client disconnection
+                clients_cache[client_name]['disconnected_at'] = datetime.datetime.now()
+                print(f"{client_name} disconnected.")
+                break
+            else:
+                # Echo message back with "ACK"
+                response = f"{data} ACK"
+                client_socket.send(response.encode())
+        except:
+            break
 
+    # Close connection and free resources
+    client_socket.close()
 
-if packet_length %4 !=0:
-    print("ERROR: The packet length is not divisible by four, exiting.")
-    serverSocket.close
-    exit()
-    
-elif pcode !=0:
-    print("ERROR: Unexpected value for pcode, exiting.")
-    serverSocket.close
-    exit() 
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+    print("Server is listening...")
 
-elif data_length != len(data):
-    print("ERROR: Length of data does not match, exiting.")
-    serverSocket.close
-    exit()
+    client_count = 0
 
-#Creating server side packet
-repeat = 6
-udp_port = 25000
-server_len = 12
-codeA = 237
-server_entity = 2
-serverPacket = struct.pack("!IHHIIHH",data_length, pcode, server_entity, repeat, udp_port, server_len, codeA)
-serverSocket.sendto(serverPacket, clientAddress)
+    while True:
+        if client_count < MAX_CLIENTS:
+            # Accept new client connection
+            client_socket, addr = server_socket.accept()
 
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(("", udp_port))
-print("NEW SERVER: udp_port ready to receive: {}".format(udp_port))
+            client_count += 1
+            client_name = f"Client{client_count:02d}"  # Assign client a name like Client01, Client02, etc.
 
-print("\nSERVER: ----------End of Stage A----------")
-print("\nSERVER: ----------Start of Stage B----------")
+            # Send client name to the client
+            client_socket.send(client_name.encode())
 
-for acked_id in range(repeat):
-    #Receiving client side Phase B packet
-    newPacket, clientAddress = serverSocket.recvfrom(1024)
-    # data, clientAddress = serverSocket.recvfrom(1024)
-    # packet_length = len(newPacket)
-    data_length, pcode, entity, packet_id = struct.unpack('!IHHI',newPacket[:12])
-    data = struct.unpack("!{}s".format(data_length), newPacket[12:])
-    print("FROM UDP CLIENT--- pcode: {} entity: {} packet_id: {} data_length: {} data: {}".format(pcode, entity,packet_id, data_length, data))
+            # Start a new thread to handle the client
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_name))
+            client_thread.start()
+        else:
+            print("Max clients connected. No more clients can connect at this time.")
 
-    #Verifying Client side packet
-    if data_length % 4 != 0:
-        print("ERROR: Data is not divisible by 4, exiting.")
-        serverSocket.close()
-        exit()
-    elif pcode != codeA:
-        print("ERROR: Unexpected pcode, exiting.")
-        serverSocket.close()
-        exit()
-    elif data_length != len(data[0]):
-        print("ERROR: Data length does not match, exiting.")
-        serverSocket.close()
-        exit()
-
-    #Sending acknowledgement packet 
-    serverPacket = struct.pack('!IHHI', data_length, pcode, server_entity, acked_id)
-    serverSocket.sendto(serverPacket, clientAddress)
-
-#Creating and sending TCP server address
-tcp_port = 29450
-codeB = 367
-
-tcp_packet = struct.pack("!IHHII", data_length, pcode, entity, tcp_port, codeB)
-serverSocket.sendto(tcp_packet, clientAddress)
-print("\nSERVER: ----------End of Stage B----------")
-print("\nSERVER: ----------Start of Stage C----------")
-
-serverSocket = socket(AF_INET, SOCK_STREAM)
-
-serverSocket.bind(("", tcp_port))
-
-serverSocket.listen(5)
-
-connectionSocket, addr = serverSocket.accept()
-print('The server is ready to receive on tcp port: {}'.format(tcp_port))
-
-repeat2 = 8
-server_len2 = 70
-codeC = 380
-server_char = b"S"
-data_length = 13 
-
-packet = struct.pack("!IHHIIIc", data_length, codeB, 2, repeat2, server_len2, codeC, server_char)
-connectionSocket.send(packet)
-print("\nSERVER: ----------End of Stage C----------")
-print("\nSERVER: ----------Start of Stage D----------")
-
-for i in range (repeat2):
-    packetD = connectionSocket.recv(1048)
-    #data = connectionSocket.recv(1024)
-    data_length, pcode, entity = struct.unpack("!IHH", packetD[:8])
-    data = struct.unpack("!{}s".format(data_length), packetD[8:])
-    print("FROM TCP CLIENT--- pcode: {} entity: {} data_length: {} data: {}".format(pcode, entity, data_length, data))
-
-codeD = 234
-
-final_packet = struct.pack("!IHHI", data_length, codeC, 2, codeD)
-connectionSocket.send(final_packet)
-print("\nSERVER: ----------End of Stage D----------")
-
-serverSocket.close()  
-sys.exit()#Terminate the program after sending the corresponding data
+if __name__ == '__main__':
+    start_server()
